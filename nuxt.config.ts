e2695ba456c4089f60aef81bb0aec6bb416847ec
iwasaki <poettype73@gmail.com>
2025-06-01 02:17:37 +0900

    scssの導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ['@nuxt/content'],
+
+  css: ['@/styles/styles.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/styles/mixin.scss";',
+        }
+      }
+    }
+  }
+});
