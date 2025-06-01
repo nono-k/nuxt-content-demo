adec37dfc4fd3fee1b6125a6ea7ff3298782509e
iwasaki <poettype73@gmail.com>
2025-06-02 08:06:43 +0900

    Add Top Page

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <Header />
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 1000px;
+  margin-inline: auto;
+  padding-inline: 20px;
+  margin-top: 60px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="title">Nuxt Content Demo</h1>
+  <p class="lead">このページはNuxt Contentのデモページです。<br>ブログ一覧は<NuxtLink to="/blog">こちら</NuxtLink>になります。</p>
+</template>
+
+<style scoped lang="scss">
+.title {
+  text-align: center;
+  font-size: 2rem;
+  margin-bottom: 1.5rem;
+}
+.lead {
+  text-align: center;
+  a {
+    color: blue;
+    text-decoration: underline;
+  }
+}
+</style>
