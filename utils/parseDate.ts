0c26d3792e8375d8a2f880517e7231fe2d811b05
iwasaki <poettype73@gmail.com>
2025-06-03 00:30:28 +0900

    Add parseDate

---
+++ utils/parseDate.ts
@@ -0,0 +1,7 @@
+export const parseDate = (date: Date) => {
+  const dateObj = new Date(date);
+  const year = dateObj.getFullYear();
+  const month = dateObj.getMonth() + 1;
+  const day = dateObj.getDate();
+  return `${year}年${month}月${day}日`;
+}
